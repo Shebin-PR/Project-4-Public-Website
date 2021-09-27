e8af6140edd2b253797ecccd9bee60b06dace528
unknown <shebinpr126@gmail.com>
2021-09-27 02:19:04 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+function VideoOne () {
+    document.getElementById('first-img').style.display = 'none'
+    document.getElementById('video-one').style.display = 'block'
+    document
+      .getElementById('video-one-content')
+      .setAttribute('src', 'https://www.youtube.com/embed/I9QGnNvrmoY?autoplay=1')
+  }
+  function VideoTwo () {
+    document.getElementById('second-img').style.display = 'none'
+    document.getElementById('video-two').style.display = 'block'
+    document
+      .getElementById('video-two-content')
+      .setAttribute('src', 'https://www.youtube.com/embed/ckub1RSlhic?autoplay=1')
+  }
+  function thirdVideo () {
+    document.getElementById('third-img').style.display = 'none'
+    document.getElementById('third-video').style.display = 'block'
+    document
+      .getElementById('third-video-content')
+      .setAttribute('src', 'https://www.youtube.com/embed/xkmM6h32lnM?autoplay=1')
+  }
+  function fourthVideo () {
+    document.getElementById('fourth-img').style.display = 'none'
+    document.getElementById('fourth-video').style.display = 'block'
+    document
+      .getElementById('fourth-video-content')
+      .setAttribute('src', 'https://www.youtube.com/embed/vhG3wKGp2mc?autoplay=1')
+  }
+  function fifthVideo () {
+    document.getElementById('fifth-img').style.display = 'none'
+    document.getElementById('fifth-video').style.display = 'block'
+    document
+      .getElementById('fifth-video-content')
+      .setAttribute('src', 'https://www.youtube.com/embed/m_hx0Ebg5Yo?autoplay=1')
+  }
+  
